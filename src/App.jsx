b1c0444c330a3fa0d104b9597b650543a60e2486
ALX4458 <canardabc@gmail.com>
2026-05-22 22:57:47 +0200

    design stylé

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ export default function App() {
   return (
     <div style={styles.page}>
       <div style={styles.container}>
-        <h1 style={styles.title}>🔗 Created by <Alex></Alex></h1>
-        <p style={styles.subtitle}>Crack <Link></Link> ✨</p>
+        <h1 style={styles.title}>🔗 Created by Alex</h1>
+        <p style={styles.subtitle}>Crack Links ✨</p>
 
         {/* FORM */}
         <div style={styles.card}>
